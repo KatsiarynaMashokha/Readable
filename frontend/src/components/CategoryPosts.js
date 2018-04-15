4840361e00359b44592937043721f5eacbad8f02
Katsiaryna Mashokha <katsiarynamashokha@gmail.com>
2018-04-15 01:20:32 -0700

    feat: add functionality to upvote and downvote the post

--- frontend/src/components/CategoryPosts.js
+++ frontend/src/components/CategoryPosts.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link, withRouter } from 'react-router-dom';
-import { Panel, Glyphicon, ButtonToolbar, Button } from 'react-bootstrap';
+import { Panel, Glyphicon, Button } from 'react-bootstrap';
 import { convertUnixTime } from '../util';
+import { deletePost, upvotePost,  downvotePost} from '../actions/PostsActions';
 
 class CategoryPosts extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            edit: false
+        }
+
+        console.log(this.props.posts);
+    }
+
+    deletePost(postId) {
+        this.props.deletePost(postId);
+    }
+
+    upvote(postId) {
+        this.props.upvotePost(postId);
+    }
+
+    downvote(postId) {
+        this.props.downvotePost(postId);
+    }
+
     render() {
+
         if(this.props.location.pathname === '/') {
             return (
                 <div>{this.props.posts.length && this.props.posts.map((post, idx) => <p key={idx}>{post.title}<br/>by {post.author}  
@@ -21,12 +44,11 @@ class CategoryPosts extends Component {
                 <Panel key={idx} className='post-panel'>
                     <Panel.Heading>{post.title}</Panel.Heading>
                     <Panel.Body>{post.body}</Panel.Body>
-                    <Panel.Footer>by {post.author} on {convertUnixTime(post.timestamp)}&nbsp;&nbsp;&nbsp;{post.voteScore} <Glyphicon glyph="glyphicon glyphicon-thumbs-up"></Glyphicon>
-                    {/* <ButtonToolbar>
-                        <Button bsStyle="primary">Edit</Button>
-                        <Button bsStyle="danger">Delete</Button>
-                    </ButtonToolbar> */}
+                    <Panel.Footer>by {post.author} on {convertUnixTime(post.timestamp)}&nbsp;&nbsp;&nbsp;{post.voteScore} <Glyphicon onClick={this.upvote.bind(this, post.id)} glyph="glyphicon glyphicon-thumbs-up"></Glyphicon>&nbsp;&nbsp;<Glyphicon onClick={this.downvote.bind(this, post.id)} glyph="glyphicon glyphicon-thumbs-down"></Glyphicon>
                     </Panel.Footer>
+                    <Link to={post.category + '/edit/' + post.id} className='button'>Edit</Link>
+                    <a onClick={this.deletePost.bind(this, post.id)} className='button'>Delete</a>
+                    <Link to={post.category + '/post/' + post.id} className='button'>Read More</Link>
                 </Panel>
                 )}
             </div>
@@ -39,4 +61,11 @@ const mapStateToProps = state => ({
     posts: state.posts
 });
 
-export default withRouter(connect(mapStateToProps)(CategoryPosts));
+const mapDispatchToProps = dispatch => ({
+    deletePost: (postId) => dispatch(deletePost(postId)),
+    upvotePost: (postId) => dispatch(upvotePost(postId)),
+    downvotePost: (postId) => dispatch(downvotePost(postId)),
+    
+})
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(CategoryPosts));
