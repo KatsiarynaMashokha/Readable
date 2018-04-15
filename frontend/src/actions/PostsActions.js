8a7b3819a72200e8c1d92b55a73c0f69e80d8a1d
Katsiaryna Mashokha <katsiarynamashokha@gmail.com>
2018-04-15 01:10:38 -0700

    feat: add actions and reducers for posts

--- frontend/src/actions/PostsActions.js
+++ frontend/src/actions/PostsActions.js
@@ -19,3 +19,80 @@ export function fetchAllPosts() {
     }
 }
 
+export function createNewPost(post) {
+    return dispatch => {
+        return fetch(
+            ROOT_URL + POSTS,
+            {
+                method: 'POST',
+                headers: AUTHORIZATION_HEADERS,
+                body: JSON.stringify(post)
+            }  
+        ).then(response => response.json())
+        .then(result => {
+            dispatch({
+                type: types.CREATE_NEW_POST,
+                post: { ...result }
+            })
+        })
+    }
+}
+
+export function deletePost(postId) {
+    return dispatch => {
+        return fetch(
+            ROOT_URL + POSTS + postId,
+            {
+                method: 'DELETE',
+                headers: AUTHORIZATION_HEADERS
+
+            }
+        ).then(response => response.json())
+        .then(result => {
+            dispatch({
+                type: types.DELETE_POST,
+                result
+            })
+        })
+    }
+}
+
+export function upvotePost(postId) {
+    return dispatch => {
+        return fetch(
+            ROOT_URL + POSTS + postId,
+            {
+                method: 'POST',
+                headers: AUTHORIZATION_HEADERS,
+                body: JSON.stringify({option: 'upVote'})
+            }
+        ).then(response => response.json())
+        .then(result => {
+            console.log(result);
+            dispatch({
+                type: types.UPVOTE_POST,
+                result
+            })
+        })
+    }
+}
+
+export function downvotePost(postId) {
+    return dispatch => {
+        return fetch(
+            ROOT_URL + POSTS + postId,
+            {
+                method: 'POST',
+                headers: AUTHORIZATION_HEADERS,
+                body: JSON.stringify({option: 'downVote'})
+            }
+        ).then(response => response.json())
+        .then(result => {
+            console.log(result);
+            dispatch({
+                type: types.DOWNVOTE_POST,
+                result
+            })
+        })
+    }
+}
--- frontend/src/reducers/posts-reducers.js
+++ frontend/src/reducers/posts-reducers.js
@@ -1,9 +1,31 @@
 import * as types from '../constants/ActionTypes';
 
 export const PostsReducer = (state = [], action) => {
+    let postId;
     switch(action.type) {
         case types.FETCH_ALL_POSTS:
             return action.result;
+        case types.CREATE_NEW_POST:
+            let { post } = action;
+            console.log(state);
+            return {
+                ...state, ...state.push(post)
+            }
+        case types.DELETE_POST:
+            postId = action.result.id;
+            return state.filter(post => post.id !== postId);
+        case types.UPVOTE_POST:
+            postId = action.result.id;
+            return state.map(post => {
+                if(post.id === postId) post.voteScore++;
+                return post;
+            });
+        case types.DOWNVOTE_POST:
+            postId = action.result.id;
+            return state.map(post => {
+                if(post.id === postId) post.voteScore--;
+                return post;
+            });
         default:
             return state
     }
